8aa2a62ccb556d0feb755ad31f962e4823d998c9
Stephen Quan <squan@esri.com>
2025-07-07 10:16:12 +1000

    Improve search demo

--- samples/SQuan.Helpers.Maui.Sample/Pages/SearchPage.xaml.cs
+++ samples/SQuan.Helpers.Maui.Sample/Pages/SearchPage.xaml.cs
@@ -6,7 +6,10 @@ namespace SQuan.Helpers.Maui.Sample;
 
 public partial class SearchPage : ContentPage
 {
-	[BindableProperty] public partial string SearchText { get; set; } = "Surveys";
+	[BindableProperty] public partial string SearchText { get; set; } = "Statue of Liberty";
+	[BindableProperty, NotifyPropertyChangedFor(nameof(IsNotSearching))] public partial bool IsSearching { get; internal set; } = false;
+	public bool IsNotSearching => !IsSearching;
+
 	public ObservableCollection<object> Results { get; } = [];
 
 	public SearchPage()
@@ -18,26 +21,38 @@ public partial class SearchPage : ContentPage
 
 	async void OnSearchClicked(object sender, EventArgs e)
 	{
-		dynamic query = new ExpandoObject();
-		query.q = SearchText;
-		query.f = "json";
-		query.start = 1;
-		query.num = 50;
-		Results.Clear();
-		while (true)
+		IsSearching = true;
+		try
 		{
-			var client = new HttpClient();
-			dynamic response = await HttpClientHelper.GetAsync(client, "https://www.arcgis.com/sharing/rest/search", FormDataHelper.EncodeFormData(query));
-			System.Diagnostics.Trace.WriteLine($"Found {response.results.Count} results, next start: {response.nextStart}");
-			foreach (var result in response.results)
+			dynamic query = new ExpandoObject();
+			query.q = SearchText;
+			query.f = "json";
+			query.start = 1;
+			query.num = 50;
+			Results.Clear();
+			while (true)
 			{
-				Results.Add(result);
+				var client = new HttpClient();
+				dynamic response = await HttpClientHelper.GetAsync(client, "https://www.arcgis.com/sharing/rest/search", FormDataHelper.EncodeFormData(query));
+				System.Diagnostics.Trace.WriteLine($"Found {response.results.Count} results, next start: {response.nextStart}");
+				foreach (var result in response.results)
+				{
+					Results.Add(result);
+				}
+				if (response.nextStart == -1)
+				{
+					break;
+				}
+				query.start = response.nextStart;
 			}
-			if (response.nextStart == -1)
-			{
-				break;
-			}
-			query.start = response.nextStart;
+		}
+		catch (Exception ex)
+		{
+			await DisplayAlert("Error", ex.Message, "OK");
+		}
+		finally
+		{
+			IsSearching = false;
 		}
 	}
 }
---
+++ samples/SQuan.Helpers.Maui.Sample/Views/SearchCard.xaml.cs
@@ -0,0 +1,44 @@
+using SQuan.Helpers.Maui.Mvvm;
+
+namespace SQuan.Helpers.Maui.Sample;
+
+public partial class SearchCard : ContentView
+{
+	[BindableProperty] public partial string? ItemId { get; set; }
+	[BindableProperty] public partial string? Title { get; set; }
+	[BindableProperty] public partial string? Snippet { get; set; }
+	[BindableProperty] public partial string? Description { get; set; }
+	[BindableProperty] public partial string? ItemType { get; set; }
+	[BindableProperty] public partial string? Owner { get; set; }
+	[BindableProperty] public partial long? Modified { get; set; }
+	[BindableProperty] public partial string? Thumbnail { get; set; }
+
+	public ImageSource? ThumbnailSource
+	{
+		get
+		{
+			if (string.IsNullOrEmpty(ItemId) || string.IsNullOrEmpty(Thumbnail))
+			{
+				return null;
+			}
+
+			return ImageSource.FromUri(new Uri($"https://www.arcgis.com/sharing/rest/content/items/{ItemId}/info/{Thumbnail}"));
+		}
+	}
+
+	public SearchCard()
+	{
+		InitializeComponent();
+
+		this.PropertyChanged += (s, e) =>
+		{
+			switch (e.PropertyName)
+			{
+				case nameof(ItemId):
+				case nameof(Thumbnail):
+					OnPropertyChanged(nameof(ThumbnailSource));
+					break;
+			}
+		};
+	}
+}
