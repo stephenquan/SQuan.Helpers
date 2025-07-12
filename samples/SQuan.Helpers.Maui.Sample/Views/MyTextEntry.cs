fd84784b6c9d7ac799537739ef9bf5da26cd0906
Stephen Quan <squan@esri.com>
2025-07-12 23:19:58 +1000

    #74 Add ObservableIndexer.RemoveNulls (#75)

---
+++ samples/SQuan.Helpers.Maui.Sample/Views/MyTextEntry.cs
@@ -0,0 +1,36 @@
+﻿using SQuan.Helpers.Maui.Mvvm;
+
+namespace SQuan.Helpers.Maui.Sample;
+public partial class MyTextEntry : Entry
+{
+	[BindableProperty] public partial string? Value { get; set; }
+
+	int changing = 0;
+
+	public MyTextEntry()
+	{
+		PropertyChanged += (sender, e) =>
+		{
+			switch (e.PropertyName)
+			{
+				case nameof(Text):
+					if (changing == 0)
+					{
+						changing++;
+						Value = !string.IsNullOrEmpty(Text) ? Text : null;
+						changing--;
+					}
+					break;
+				case nameof(Value):
+					if (changing == 0)
+					{
+						changing++;
+						Text = Value;
+						changing--;
+					}
+					break;
+			}
+		};
+	}
+}
+
