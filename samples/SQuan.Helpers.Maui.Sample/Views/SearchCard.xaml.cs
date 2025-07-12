964da6944f5eb653f7b41069059f6e97193a869c
Stephen Quan <squan@esri.com>
2025-07-12 20:16:37 +1000

    #72 Add UnixTimeToDateTimeConverter (#73)

--- samples/SQuan.Helpers.Maui.Sample/Views/SearchCard.xaml.cs
+++ samples/SQuan.Helpers.Maui.Sample/Views/SearchCard.xaml.cs
@@ -13,31 +13,8 @@ public partial class SearchCard : ContentView
 	[BindableProperty] public partial long? Modified { get; set; }
 	[BindableProperty] public partial ImageSource? ThumbnailImage { get; set; }
 
-	public DateTime? ModifiedDate
-	{
-		get
-		{
-			if (Modified is long modified)
-			{
-				return DateTimeOffset.FromUnixTimeMilliseconds(modified).DateTime;
-			}
-
-			return null;
-		}
-	}
-
 	public SearchCard()
 	{
 		InitializeComponent();
-
-		this.PropertyChanged += (s, e) =>
-		{
-			switch (e.PropertyName)
-			{
-				case nameof(Modified):
-					OnPropertyChanged(nameof(ModifiedDate));
-					break;
-			}
-		};
 	}
 }
