487300b5f913abab2f745897271fc0137cc89a10
Stephen Quan <squan@esri.com>
2025-10-19 22:02:51 +1100

    Demonstrate SP spatial indexing functions working with the rtree

--- samples/SQuan.Helpers.Sample/Models/SpatialData.cs
+++ samples/SQuan.Helpers.Sample/Models/SpatialData.cs
@@ -1,7 +1,10 @@
-﻿namespace SQuan.Helpers.Sample;
+﻿// SpatialData.cs
+
+namespace SQuan.Helpers.Sample;
 
 public partial class SpatialData
 {
+	public int Id { get; set; }
 	public string Name { get; set; } = string.Empty;
 	public string Color { get; set; } = "Black";
 	public string Geometry { get; set; } = string.Empty;
