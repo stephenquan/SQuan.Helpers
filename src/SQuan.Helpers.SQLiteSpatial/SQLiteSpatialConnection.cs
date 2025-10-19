e27464cd34b6473f818811c36110ca2391da36f3
Stephen Quan <squan@esri.com>
2025-10-19 16:23:46 -0700

    #104 Improve XML docs (#105)

--- src/SQuan.Helpers.SQLiteSpatial/SQLiteSpatialConnection.cs
+++ src/SQuan.Helpers.SQLiteSpatial/SQLiteSpatialConnection.cs
@@ -1,20 +1,28 @@
-﻿using SQLite;
+﻿// SQLiteSpatialConnection.cs
+
+using SQLite;
 
 namespace SQuan.Helpers.SQLiteSpatial;
 
 /// <summary>
-/// Adds spatial capabilities to SQLiteConnection.
+/// Represents a specialized <see cref="SQLiteConnection"/> that enables spatial capabilities for SQLite databases.
+/// This class automatically enables spatial extensions upon initialization, allowing spatial queries and operations.
 /// </summary>
 public partial class SQLiteSpatialConnection : SQLiteConnection
 {
 	/// <summary>
 	/// Initializes a new instance of the <see cref="SQLiteSpatialConnection"/> class.
+	/// Enables spatial extensions for the underlying SQLite database connection.
 	/// </summary>
-	/// <param name="databasePath"></param>
-	/// <param name="openFlags"></param>
+	/// <param name="databasePath">The file path to the SQLite database.</param>
+	/// <param name="openFlags">
+	/// The flags controlling how the database is opened. Defaults to <see cref="SQLiteOpenFlags.ReadWrite"/>, 
+	/// <see cref="SQLiteOpenFlags.Create"/>, and <see cref="SQLiteOpenFlags.FullMutex"/>.
+	/// </param>
 	public SQLiteSpatialConnection(string databasePath, SQLiteOpenFlags openFlags = SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create | SQLiteOpenFlags.FullMutex)
 		: base(databasePath, openFlags)
 	{
+		// Enable spatial extensions for this connection.
 		SQLiteSpatialExtensions.EnableSpatialExtensions(this);
 	}
 }
