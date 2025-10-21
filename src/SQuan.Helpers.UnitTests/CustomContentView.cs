bb3b1ab4136974d47c3585811e563b45e3d535d6
Stephen Quan <squan@esri.com>
2025-10-21 16:55:46 +1100

    Raised OnPropertyChanged() for initializers

---
+++ src/SQuan.Helpers.UnitTests/CustomContentView.cs
@@ -0,0 +1,15 @@
+﻿// CustomContentView.cs
+
+using SQuan.Helpers.Maui.Mvvm;
+
+namespace SQuan.Helpers.Maui.UnitTests;
+
+public partial class CustomContentView : ContentView
+{
+	[BindableProperty] public partial int Magic { get; set; } = 42;
+	public int MagicChangedCount { get; private set; } = 0;
+	partial void OnMagicChanged(int oldValue, int newValue)
+	{
+		MagicChangedCount++;
+	}
+}
---
+++ src/SQuan.Helpers.UnitTests/MvvmUnitTests.cs
@@ -0,0 +1,20 @@
+﻿// MvvmUnitTests.cs
+
+using SQuan.Helpers.Maui.UnitTests.Mocks;
+
+namespace SQuan.Helpers.Maui.UnitTests;
+
+public class MvvmUnitTests
+{
+	[Fact]
+	public void OnPropertyChanged_WhenBindablePropertyInitialized_EventCountIsCorrect()
+	{
+		DispatcherProvider.SetCurrent(new MockDispatcherProvider());
+		var view = new CustomContentView();
+		Assert.Equal(42, view.Magic);
+		Assert.Equal(1, view.MagicChangedCount);
+		view.Magic = 100;
+		Assert.Equal(100, view.Magic);
+		Assert.Equal(2, view.MagicChangedCount);
+	}
+}
