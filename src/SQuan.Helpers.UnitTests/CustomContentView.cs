30dd05a96c4b8d1bb12f144bdd7c53a1c3ae6de0
Stephen Quan <squan@esri.com>
2025-10-22 19:22:41 +1100

    Emit initial BindableProperty change events for non-default initializers

--- src/SQuan.Helpers.UnitTests/CustomContentView.cs
+++ src/SQuan.Helpers.UnitTests/CustomContentView.cs
@@ -1,5 +1,6 @@
 ﻿// CustomContentView.cs
 
+using System.Globalization;
 using SQuan.Helpers.Maui.Mvvm;
 
 namespace SQuan.Helpers.Maui.UnitTests;
@@ -7,9 +8,9 @@ namespace SQuan.Helpers.Maui.UnitTests;
 public partial class CustomContentView : ContentView
 {
 	[BindableProperty] public partial int Magic { get; set; } = 42;
+	[BindableProperty] public partial CultureInfo? Culture { get; set; }
 	public int MagicChangedCount { get; private set; } = 0;
-	partial void OnMagicChanged(int oldValue, int newValue)
-	{
-		MagicChangedCount++;
-	}
+	public int CultureChangedCount { get; private set; } = 0;
+	partial void OnMagicChanged(int value) => MagicChangedCount++;
+	partial void OnCultureChanged(CultureInfo? value) => CultureChangedCount++;
 }
