7a17ca86a5be862ecbd44311f4fec530119bbe49
Stephen Quan <squan@esri.com>
2025-11-03 09:55:23 +1100

    Implement CoerceValue property

--- src/SQuan.Helpers.Maui.Mvvm/BindablePropertyAttribute.cs
+++ src/SQuan.Helpers.Maui.Mvvm/BindablePropertyAttribute.cs
@@ -1,4 +1,6 @@
-﻿namespace SQuan.Helpers.Maui.Mvvm;
+﻿// BindablePropertyAttribute.cs
+
+namespace SQuan.Helpers.Maui.Mvvm;
 
 /// <summary>
 /// Indicates that a property is bindable, allowing it to be used in data binding scenarios.
@@ -6,6 +8,13 @@
 [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
 public class BindablePropertyAttribute : Attribute
 {
-	/// <summary>Gets or sets the default binding mode for the bindable property.</summary>
+	/// <summary>
+	/// Gets or sets the default binding mode for the bindable property.
+	/// </summary>
 	public string DefaultBindingMode { get; set; } = "OneWay";
+
+	/// <summary>
+	/// Gets or sets the method name that will be used for CoerceValue logic.
+	/// </summary>
+	public string CoerceValue { get; set; } = string.Empty;
 }
