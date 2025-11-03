8f43d9762ad7b2da7bc9b8990e6b033dde1adb34
Stephen Quan <squan@esri.com>
2025-11-03 15:42:00 -0800

    #8 Add bool LocalizationManager.Current.FollowedInstalledUICulture = true (#148)

--- samples/SQuan.Helpers.Sample/Extensions/EvalExtension.cs
+++ samples/SQuan.Helpers.Sample/Extensions/EvalExtension.cs
@@ -1,43 +1,76 @@
-﻿using SQuan.Helpers.Maui.Mvvm;
+﻿// EvalExtension.cs
+
+using CommunityToolkit.Maui.Converters;
+using SQuan.Helpers.Maui.Mvvm;
 
 namespace SQuan.Helpers.Sample;
 
+/// <summary>
+/// A markup extension that evaluates a mathematical expression and provides the result as a binding.
+/// </summary>
+/// <remarks>The <see cref="EvalExtension"/> class allows you to define a mathematical expression as a string
+/// and bind its result to a property.
+/// The expression can reference up to six variables (X0 through X5), which can be dynamically updated.
+/// This extension is useful for scenarios where calculated values need to be bound in XAML.</remarks>
 [ContentProperty(nameof(Expression))]
 [RequireService([typeof(IReferenceProvider), typeof(IProvideValueTarget)])]
 public partial class EvalExtension : BindableObject, IMarkupExtension<BindingBase>
 {
+	readonly MultiMathExpressionConverter multiMathExpressionConverter = new MultiMathExpressionConverter();
+
+	/// <summary>Gets or sets the mathematical expression to evaluate.</summary>
 	[BindableProperty, NotifyPropertyChangedFor(nameof(ReturnValue))] public partial string Expression { get; set; } = string.Empty;
+
+	/// <summary>Gets or sets the value of variable X0 in the expression.</summary>
 	[BindableProperty, NotifyPropertyChangedFor(nameof(ReturnValue))] public partial object? X0 { get; set; } = null;
+
+	/// <summary>Gets or sets the value of variable X1 in the expression.</summary>
 	[BindableProperty, NotifyPropertyChangedFor(nameof(ReturnValue))] public partial object? X1 { get; set; } = null;
+
+	/// <summary>Gets or sets the value of variable X2 in the expression.</summary>
 	[BindableProperty, NotifyPropertyChangedFor(nameof(ReturnValue))] public partial object? X2 { get; set; } = null;
+
+	/// <summary>Gets or sets the value of variable X3 in the expression.</summary>
 	[BindableProperty, NotifyPropertyChangedFor(nameof(ReturnValue))] public partial object? X3 { get; set; } = null;
+
+	/// <summary>Gets or sets the value of variable X4 in the expression.</summary>
 	[BindableProperty, NotifyPropertyChangedFor(nameof(ReturnValue))] public partial object? X4 { get; set; } = null;
+
+	/// <summary>Gets or sets the value of variable X5 in the expression.</summary>
 	[BindableProperty, NotifyPropertyChangedFor(nameof(ReturnValue))] public partial object? X5 { get; set; } = null;
 
-	public MathParser Parser { get; } = new MathParser();
-
+	/// <summary>Gets the result of evaluating the mathematical expression.</summary>
 	public object? ReturnValue
 	{
 		get
 		{
-			if (Parser.Expression != this.Expression)
+			if (string.IsNullOrEmpty(this.Expression))
 			{
-				if (!Parser.Prepare(this.Expression))
-				{
-					return null;
-				}
+				return null;
 			}
-			return Parser.Evaluate(new object?[] { X0, X1, X2, X3, X4, X5 });
+
+			try
+			{
+				return multiMathExpressionConverter.Convert(
+					new object?[] { X0, X1, X2, X3, X4, X5 },
+					typeof(object),
+					this.Expression,
+					System.Globalization.CultureInfo.InvariantCulture);
+			}
+			catch (Exception ex)
+			{
+				return $"Exception: {ex.Message}";
+			}
 		}
 	}
 
 	/// <summary>
-	/// Provides a binding object based on the specified service provider.
+	/// Provides the binding for the evaluated expression.
+	/// Sets the extension's BindingContext to the target object's BindingContext,
+	/// then returns a one-way binding to the <see cref="ReturnValue"/> property.
 	/// </summary>
-	/// <param name="serviceProvider">An object that provides services for the binding. This parameter is typically used to resolve services or context
-	/// information required to create the binding.</param>
-	/// <returns>A <see cref="BindingBase"/> instance that represents the binding to be used. The specific type and configuration of
-	/// the binding depend on the implementation.</returns>
+	/// <param name="serviceProvider">Service provider for markup extension context.</param>
+	/// <returns>A <see cref="BindingBase"/> that binds to the evaluated result.</returns>
 	BindingBase IMarkupExtension<BindingBase>.ProvideValue(IServiceProvider serviceProvider)
 	{
 		if (serviceProvider.GetService(typeof(IProvideValueTarget)) is IProvideValueTarget provideValueTarget)
@@ -50,6 +83,14 @@ public partial class EvalExtension : BindableObject, IMarkupExtension<BindingBas
 		return BindingBase.Create(static (EvalExtension ctx) => ctx.ReturnValue, BindingMode.OneWay, source: this);
 	}
 
+	/// <summary>
+	/// Provides the binding for the evaluated expression for non-generic markup extension usage.
+	/// This method delegates to the generic <see cref="IMarkupExtension{T}.ProvideValue(IServiceProvider)"/> implementation.
+	/// </summary>
+	/// <param name="serviceProvider">Service provider for markup extension context.</param>
+	/// <returns>
+	/// A <see cref="BindingBase"/> that binds to the evaluated result of the mathematical expression.
+	/// </returns>
 	object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
 		=> (this as IMarkupExtension<BindingBase>).ProvideValue(serviceProvider);
 }
--- samples/SQuan.Helpers.Sample/Pages/LocalizePage.xaml.cs
+++ samples/SQuan.Helpers.Sample/Pages/LocalizePage.xaml.cs
@@ -1,3 +1,5 @@
+// LocalizePage.xaml.cs
+
 using System.Globalization;
 using SQuan.Helpers.Maui.Localization;
 using SQuan.Helpers.Maui.Mvvm;
@@ -5,16 +7,30 @@ using RelayCommandAttribute = CommunityToolkit.Mvvm.Input.RelayCommandAttribute;
 
 namespace SQuan.Helpers.Sample;
 
+/// <summary>
+/// Demonstrates localization feature with <see cref="LocalizationManager"/>.
+/// </summary>
 public partial class LocalizePage : ContentPage
 {
-	[ObservableProperty] public partial int Count { get; set; } = 0;
+	/// <summary>Simple counter to demonstrate localized string with pluralization.</summary>
+	[ObservableProperty] public partial int Count { get; internal set; } = 0;
 
-	[ObservableProperty] public partial double? X1 { get; set; } = 10.0;
+	/// <summary>Get or set the first variable (X0) for expression evaluation.</summary>
 
+	[ObservableProperty] public partial double? X0 { get; set; } = 3.0;
+
+	/// <summary>Get or set the second variable (X1) for expression evaluation.</summary>
+	[ObservableProperty] public partial double? X1 { get; set; } = 4.0;
+
+	/// <summary>Get or set the mathematical expression to evaluate.</summary>
 	[ObservableProperty] public partial string Expression { get; set; } = "x0 * x1";
 
-	[ObservableProperty] public partial DateTime? Modified { get; set; }
+	/// <summary>Get or set the user date.</summary>
+	[ObservableProperty] public partial DateTime? UserDate { get; set; }
 
+	/// <summary>
+	/// Get the list of supported cultures for localization.
+	/// </summary>
 	public List<CultureInfo> SupportedCultures { get; } =
 	[
 		new CultureInfo("en-US"),
@@ -23,9 +39,13 @@ public partial class LocalizePage : ContentPage
 		new CultureInfo("ar-AR"),
 	];
 
+	/// <summary>Get or set the current culture index in the <see cref="SupportedCultures"/> list.</summary>
 	public int CultureIndex { get; set; } = 0;
 
+	/// <summary>Get the current <see cref="LocalizationManager"/> instance.</summary>
 	public LocalizationManager LM { get; } = LocalizationManager.Current;
+
+	/// <summary>Initializes a new instance of the <see cref="LocalizePage"/> class.</summary>
 
 	public LocalizePage()
 	{
@@ -60,9 +80,17 @@ public partial class LocalizePage : ContentPage
 		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
 
+	/// <summary>Cycles through the supported cultures and changes the current UI culture.</summary>
 	[RelayCommand]
-	void ChangeUICulture()
+	public void ToggleCulture()
 	{
 		LM.CurrentUICulture = SupportedCultures[CultureIndex = (CultureIndex + 1) % SupportedCultures.Count];
+	}
+
+	/// <summary>Sets the current UI culture to the system's installed UI culture.</summary>
+	[RelayCommand]
+	public void SystemCulture()
+	{
+		LM.FollowInstalledUICulture = true;
 	}
 }
