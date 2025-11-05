bee2005f3b66572f7c430fb6be2d15ccf95e24a1
Stephen Quan <squan@esri.com>
2025-11-05 22:11:45 +1100

    Add unit tests

---
+++ src/SQuan.Helpers.Maui.UnitTests/LucideTests.cs
@@ -0,0 +1,31 @@
+﻿// LucideTests.cs
+
+namespace SQuan.Helpers.Maui.UnitTests;
+
+public class LucideTests
+{
+	[Fact]
+	public void LucideIcons_FontFile_IsATrueTypeFontFileName()
+	{
+		Assert.NotNull(SQuan.Helpers.Maui.LucideIcons.FontFile);
+		Assert.NotEmpty(SQuan.Helpers.Maui.LucideIcons.FontFile);
+		Assert.EndsWith(".ttf", SQuan.Helpers.Maui.LucideIcons.FontFile, StringComparison.OrdinalIgnoreCase);
+	}
+
+	[Fact]
+	public void LucideIcons_FontFamily_IsNotNullOrEmpty()
+	{
+		Assert.NotNull(SQuan.Helpers.Maui.LucideIcons.FontFamily);
+		Assert.NotEmpty(SQuan.Helpers.Maui.LucideIcons.FontFamily);
+	}
+
+	[Fact]
+	public void LucideIcons_SmileAndGlobe_AreDistinctIcons()
+	{
+		Assert.NotNull(SQuan.Helpers.Maui.LucideIcons.Smile);
+		Assert.NotNull(SQuan.Helpers.Maui.LucideIcons.Globe);
+		Assert.NotEmpty(SQuan.Helpers.Maui.LucideIcons.Smile);
+		Assert.NotEmpty(SQuan.Helpers.Maui.LucideIcons.Globe);
+		Assert.NotEqual(SQuan.Helpers.Maui.LucideIcons.Smile, SQuan.Helpers.Maui.LucideIcons.Globe);
+	}
+}
