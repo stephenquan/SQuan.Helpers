af22d34007a10226151b2bdb49222f35c5b4203d
Stephen Quan <squan@esri.com>
2026-02-18 22:50:32 +1100

    Improve unit test

--- src/SQuan.Helpers.Maui.UnitTests/AspectViewTests.cs
+++ src/SQuan.Helpers.Maui.UnitTests/AspectViewTests.cs
@@ -7,13 +7,16 @@ namespace SQuan.Helpers.Maui.UnitTests;
 public class AspectViewTests
 {
 	[Fact]
-	public void AspectView_WithChild_RespectsAspectRatio()
+	public async Task AspectView_WithChild_RespectsAspectRatio()
 	{
 		DispatcherProvider.SetCurrent(new MockDispatcherProvider());
 		var aspectView = new AspectView() { WidthRequest = 300, HeightRequest = 200 };
+		TaskCompletionSource<bool> tcs = new();
 		var grid = new Grid();
+		grid.Loaded += (s, e) => tcs.SetResult(true);
 		aspectView.Content = grid;
 		Assert.Equal(1, aspectView.AspectRatio);
+		await tcs.Task;
 		Assert.Equal(200, grid.Width);
 		Assert.Equal(200, grid.Height);
 	}
