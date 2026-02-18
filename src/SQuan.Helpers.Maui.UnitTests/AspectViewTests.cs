dada59ec046d057e0990966d39a5a0d273caa4ae
Stephen Quan <squan@esri.com>
2026-02-18 21:41:39 +1100

    Add AspectViewTests (wip)

---
+++ src/SQuan.Helpers.Maui.UnitTests/AspectViewTests.cs
@@ -0,0 +1,20 @@
+﻿// AspectViewTests.cs
+
+using SQuan.Helpers.Maui.UnitTests.Mocks;
+
+namespace SQuan.Helpers.Maui.UnitTests;
+
+public class AspectViewTests
+{
+	[Fact]
+	public void AspectView_WithChild_RespectsAspectRatio()
+	{
+		DispatcherProvider.SetCurrent(new MockDispatcherProvider());
+		var aspectView = new AspectView() { WidthRequest = 300, HeightRequest = 200 };
+		var grid = new Grid();
+		aspectView.Content = grid;
+		Assert.Equal(1, aspectView.AspectRatio);
+		Assert.Equal(200, grid.Width);
+		Assert.Equal(200, grid.Height);
+	}
+}
